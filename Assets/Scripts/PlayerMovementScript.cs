d26f1f4741980c3958c878b2ed56d55a8fc678c9
Kasper Sekuła <kasper.sekula@gmail.com>
2021-11-29 14:30:50 +0100

    Added user input.

---
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovementScript : MonoBehaviour
+{
+    private Camera mainCamera;
+    
+    void Start()
+    {
+        mainCamera = Camera.main;
+    }
+
+
+    void Update()
+    {
+        if(Touchscreen.current.primaryTouch.press.isPressed)
+        {
+            Vector2 touchPosition = Touchscreen.current.primaryTouch.position.ReadValue();
+            Debug.Log(touchPosition);
+            Vector3 worldPosition = mainCamera.ScreenToWorldPoint(touchPosition);
+            Debug.Log(worldPosition);
+        }
+    }
+}
